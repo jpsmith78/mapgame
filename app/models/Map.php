445f9216cbe4819231046b7e9111353d78239b5a
Justin Smith <justinpetersmith@gmail.com>
2020-04-12 18:44:39 -0400

    added js fuction to add visited class to states based on database

--- app/models/Map.php
+++ app/models/Map.php
@@ -7,13 +7,18 @@
     }
 
     public function getMaps(){
-      $this->db->query('SELECT u.name AS user, s.name AS state
+      $this->db->query('SELECT DISTINCT u.name AS user_name, s.name AS state_name, s.abbreviation as state_abbr
         FROM users_states us
         INNER JOIN states s ON s.id = us.state_id
-        INNER JOIN users u ON u.id = us.user_id');
+        INNER JOIN users u ON u.id = us.user_id
+        WHERE u.name = :user_name');
+
+      $this->db->bind(':user_name', $_SESSION['user_name']);
 
       $results = $this->db->resultSet();
       return $results;
     }
+
+
 
   }
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
+function toggleVisited(){
+  var element = event.target;
+  if (!element.classList.contains("visited")) {
+    element.classList.add("visited");
+  } else {
+    element.classList.remove("visited");
+  }
+  console.log(element);
+}
 
+function findVisitedStates(values){
+  for (var i = 0; i < values.length; i++) {
+    if(!document.getElementById(values[i]).classList.contains("visited")){
+      document.getElementById(values[i]).classList.add("visited");
+    }
+  }
+}
